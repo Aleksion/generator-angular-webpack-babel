de56332a1e48d7640e4128fbb8923ab4fcc81895
Aleksander Herforth Rendtslev <aleksanderhr@live.com>
2015-12-11 00:36:52 +0100

    fixed config

--- generators/app/content/webpack.config.js
+++ generators/app/content/webpack.config.js
@@ -6,8 +6,8 @@ program
   .option('-inline, --inline', 'Whether to run with auto refresh')
   .option('-p, --production', 'Whether to run for production')
   .option('-bail, --bail', 'Whether to bail on error')
-  .option('-build, --build <build>', 'Whether to run for build', false)
-  .option('-test, --test <test>', 'Whether to run for test', false)
+  .option('-build, --build', 'Whether to run for build')
+  .option('-test, --test', 'Whether to run for test')
   .option('-type, --type <type>', 'The environment to run it in', 'local')
   .parse(process.argv);
 
